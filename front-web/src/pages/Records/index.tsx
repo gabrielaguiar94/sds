a99edf27e0d5f5b6f5aff0aa7fef94893c5e722a
Gabriel Aguiar <gabrielaguiar1994@hotmail.com>
2020-09-18 22:21:57 -0300

    Configurando front-web para produção

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import Filters from '../../components/Filters'
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://sds1-gabriel-aguiar.herokuapp.com';
 
 const Records = () => {
     const [recordsResponse, setRecordResponse] = useState<RecordsResponse>();
